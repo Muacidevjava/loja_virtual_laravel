a9d0f343de482148995ea62f85d8e066c51b25e5
MuaciDevjava <muaci@yahoo.com>
2025-04-23 19:10:17 -0300

    tabela-unidade-configurando-model

--- projeto-loja-admin/database/seeders/DatabaseSeeder.php
+++ projeto-loja-admin/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Unidade;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
 
             StatusSeeder::class,
             CategoriaSeeder::class,
+            UnidadeSeeder::class,
 
         ]);
 
