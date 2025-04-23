68dc00dec149b389b8d44d9c4e60f98916751bb6
MuaciDevjava <muaci@yahoo.com>
2025-04-23 16:11:00 -0300

    Tabela Status - Mostrando Dados

--- projeto-loja-admin/resources/views/template.blade.php
+++ projeto-loja-admin/resources/views/template.blade.php
@@ -19,7 +19,7 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
             integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
-        <script src="js/jquery.min.js"></script>
+        <script src="{{ asset ('assets/js/jquery.min.js') }}"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
         <script>
--- projeto-loja-admin/routes/web.php
+++ projeto-loja-admin/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Cadastro\StatusController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::get('/', [HomeController::class, "index"] )->name("home");
+
+Route::resource("/status", StatusController::class);
