0e86245a9b6e8bfd1b8c0d809b241566c0bb7ef2
MuaciDevjava <muaci@yahoo.com>
2025-04-26 20:03:08 -0300

    Criando o Movimento Observer e registrando no Provider, e Criando Metodos

--- projeto-loja-admin/app/Providers/AppServiceProvider.php
+++ projeto-loja-admin/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Movimento;
+use App\Observers\MovimentoObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Movimento::observe(MovimentoObserver::class);
     }
 }
